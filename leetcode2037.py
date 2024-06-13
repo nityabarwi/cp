676efd27131862587bd332ae29ff0ac664267dcb
NITYA BARWI <95531972+nityabarwi@users.noreply.github.com>
2024-06-13 16:54:11 +0530

    Update leetcode2037.py

--- leetcode2037.py
+++ leetcode2037.py
@@ -1,1 +1,9 @@
+def minMovesToSeat(seats, students):
+        seats.sort()
+        students.sort()
+        moves = 0
+        for seat, student in zip(seats, students):
+            moves += abs(seat - student)
+        return moves
+
 
