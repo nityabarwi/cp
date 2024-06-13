6d1b58a8b6aed1b3578de84a63495a1a8804bfea
NITYA BARWI <95531972+nityabarwi@users.noreply.github.com>
2024-06-13 16:56:01 +0530

    Update leetcode2037.py

--- leetcode2037.py
+++ leetcode2037.py
@@ -6,4 +6,6 @@ def minMovesToSeat(seats, students):
             moves += abs(seat - student)
         return moves
 
-
+seats1 = [3,1,5]
+students1 = [2,7,4]
+print(minMovesToSeat(seats1, students1)              # Output = 4
